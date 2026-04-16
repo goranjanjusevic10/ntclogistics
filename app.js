87a31eb2b9be5b96954c309f22576a6e7ff1ab00
goranjanjusevic10 <goran.janjusevic@ntclogistics.me>
2026-04-16 12:59:55 +0200

    Add JSON middleware and order endpoint

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Middleware radi')
+})
+
+app.post('/shopify/order', (req, res) => {
+  console.log('ORDER RECEIVED:', req.body)
+  res.sendStatus(200)
 })
 
 const PORT = process.env.PORT || 3000
